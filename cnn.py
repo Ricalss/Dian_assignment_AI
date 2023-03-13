375cf40896f8b934131b1a54a9cca4612262b3a7
fengxiongfei <2512984312@qq.com>
2023-03-13 17:25:29 +0800

    to be finished

--- cnn.py
+++ cnn.py
@@ -1,51 +1,85 @@
-### level 1
-#配置Python环境，如果个人笔记本有高性能GPU（即显卡），可以安装cuda使用GPU加速训练。安装torch、pandas、numpy等常用数据处理、机器学习的相关库。
-#在cnn.py文件中使用**torch**库完成**mnist数据集**手写数字识别（至少包含卷积层、线性层，损失函数使用交叉熵，准确率达到90%以上）
-import pandas
-import numpy as np
 import torch
-import tqdm
+import torch.nn as nn
+import torchvision
+from torch.autograd import Variable
+import torch.utils.data as Data
+import torchvision.transforms as transforms
+#CNN框架
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1,padding=2), #卷积层（输入通道数，卷积核个数，卷积核尺寸，卷积核步长,外延填充数）
+            nn.BatchNorm2d(16),             #对这16个结果进行规范处理,卷积网络中(防止梯度消失或爆炸)，设置的参数就是卷积的输出通道数
+            nn.ReLU(),    #激活函数
+            nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer3 = nn.Linear(7*7*32, 10)
+    def forward(self,x):
+        x=self.layer1(x)
+        x=self.layer2(x)
+        x = x.view(x.size(0), -1)  # TODO what
+        x=self.layer3(x)
+        return x
 
-def load_mnist(root ='./Dian2023/MNIST_data/', n_samples=int(6e4)):
+#超参数
+num_epochs = 5
+BATCH_SIZE = 128
+learning_rate = 0.001
+moment = 0
 
-    # 读取标签数据集        
-    with open(root + 'train-labels.idx1-ubyte', 'rb') as labelpath:
-        labels_train = torch.tensor(np.fromfile(labelpath, dtype=np.uint8))
-    with open(root + 't10k-labels.idx1-ubyte', 'rb') as labelpath:
-        labels_test = torch.tensor(np.fromfile(labelpath, dtype=np.uint8))
-    # 读取图像数据集
-    rows = 28
-    cols = 28
-    with open(root + 'train-images.idx3-ubyte', 'rb') as imagepath:
-        images_train = torch.tensor(np.fromfile(imagepath, dtype=np.uint8).reshape(60000,rows*cols),dtype = torch.float)
-    with open(root + 'train-images.idx3-ubyte', 'rb') as imagepath:
-        images_test = torch.tensor(np.fromfile(imagepath, dtype=np.uint8).reshape(10000,rows*cols),dtype = torch.float)
-    return (images_train, labels_train, images_test, labels_test, rows*cols)
-#损失函数 交叉熵 下使用torch.CrossEntropyLoss()
-criterion = torch.nn.CrossEntropyLoss()  
+# 读取图像数据集
+train_data = torchvision.datasets.MNIST(root ='./mnist',train = True,transform=transforms.ToTensor(),download = True )
+train_loader=Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+# 读取标签数据集   
 
-#optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)  torch自带优化器,本次不使用，采用小样本训练
+test_data=torchvision.datasets.MNIST(root='./mnist', train=False,transform=transforms.ToTensor())
+test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=1, shuffle=False)
+"""with torch.no_grad(): #取消计算图，避免内存消耗
+    images_test=Variable(torch.unsqueeze(test_data.data, dim=1)).type(torch.FloatTensor)[:]/255   
+    labels_test=test_data.targets[:]"""
 
-images_train, labels_train, images_test, labels_test, scale = load_mnist()
-#初始化weight
-weight = torch.randn(10,scale)
 
-#优化步进
-step_size = 0.01
 
-#小样本训练
-times =0
-while tqdm(times==100):
-    data_batch = torch.sample_training_data(images_train, 256) #  256个样本
-    weights_grad = torch.evaluate_gradient(criterion, data_batch, weight)
-    weight += - step_size * weights_grad
-    times += 1
+#实例化CNN模型
+model = CNN()
+model.cuda()
 
-#正确率评判
+#损失函数 交叉熵 
+Lossfunc = nn.CrossEntropyLoss()  
+#优化函数有Adam和SGD
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
+
+#开始训练模型
+for epoch in range(num_epochs):
+    #加载训练数据
+    for step,(x,y) in enumerate(train_loader):
+        #分别得到训练数据的x和y的取值
+        ima_train=Variable(x).cuda()
+        lab_train=Variable(y).cuda()
+        
+        optimizer.zero_grad() 
+        output=model(ima_train)         #调用模型预测
+        loss=Lossfunc(output,lab_train) #计算损失值
+        loss.backward()         #反向传播
+        optimizer.step()          #梯度下降
+ 
+        #每执行50次，输出一下当前epoch、loss、accuracy
+        if(step+1) % 100 == 0:
+            print('Epoch [%d/%d], Iter[%d/%d] Loss: %.4f' %(epoch+1, num_epochs, +1, len(train_data)/BATCH_SIZE, loss.item()))
+
+model.eval()  #Change model to 'eval' mode (BN uses moving mean/var).
 correct = 0
-for i in range(len(images_test)):
-    if labels_test[i] == torch.argmax(torch.mm(weight,images_test[i])):
-        correct += 1
-print('==> correct:', correct)
-print('==> total:', len(images_test))
-print('==> acc:', correct / len(images_test))
+total = 0
+for images, labels in test_loader:
+    images = Variable(images)
+    outputs = model(images)
+    predicted = torch.max(outputs.data, 1)  #按照维度取最大值，返回每一行中最大的元素，且返回索引
+    total += labels.size()         #labels.size(0) = 100 = batch_size
+    correct += (predicted.cpu() == labels).sum()  #计算每次批量处理后，100个测试图像中有多少个预测正确，求和加入correct
+       
+print('Test accuracy of the model on the 10000 test images: %d %%' %(100 * correct/total))
